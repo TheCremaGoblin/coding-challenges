822f954f1f2d02eec257122b4b01481efc44220f
Marcus Izaquias <mizaquias@hotmail.co.uk>
2022-11-16 15:05:38 +0000

    7 Kyu solution

---
+++ codewars/7-kyu/shortest-word.js
@@ -0,0 +1,11 @@
+//Given a string of words, return the length of the shortest word(s). String will never be empty and you do not need to account for different data types.
+
+function findShort(s){
+    return s.split(' ').sort((a,b) => a.length - b.length)[0].length;
+  }
+
+//2nd attempt
+
+function findShort(s){
+    return Math.min(...s.split(" ").map (s => s.length));
+}
