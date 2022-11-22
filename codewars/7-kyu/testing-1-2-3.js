9ac34bae1a1f328dafa9e0c859bc0977911862b2
Marcus <mizaquias@hotmail.co.uk>
2022-11-22 21:20:18 +0000

    Create testing-1-2-3.js
    another 7 kyu with some refactoring afterwards

---
+++ codewars/7-kyu/testing-1-2-3.js
@@ -0,0 +1,20 @@
+//Write a function which takes a list of strings and returns each line prepended by the correct number.
+//The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+//[] --> []
+//["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+const number = function(array){
+    let test = array.map((num, i) => {
+      let change = `${i + 1}: `
+      return change += num  
+    })
+    return test
+  }
+
+  //second attempt, some refactoring
+
+const number = function(array) {
+    return array.map(function (line, index) {
+      return (index + 1) + ": " + line;
+    });
+}
