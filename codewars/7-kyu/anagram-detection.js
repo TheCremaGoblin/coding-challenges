7fabeba3c56eee4947e3a3c81ebefce8fcc4d0fd
Marcus Izaquias <mizaquias@hotmail.co.uk>
2023-01-20 20:52:29 +0000

    7 kyu solution

--- codewars/7-kyu/anagram-detection.js
+++ codewars/7-kyu/anagram-detection.js
@@ -12,5 +12,5 @@
 var isAnagram = function(test, original) {
   var t = test.toLowerCase().split('').sort().join('');
   var o = original.toLowerCase().split('').sort().join('');
-  return (t==o)?true:false;;;
+  return (t==o)?true:false;
 };
