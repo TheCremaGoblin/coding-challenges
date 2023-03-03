6cbb5101eb1a8785f054544bfc373d0f1d00cadf
Marcus <mizaquias@hotmail.co.uk>
2023-03-03 18:13:28 +0000

    Update ;ario-and-muigi-pipe-problem.js
    test submit, tablet

--- codewars/8-kyu/;ario-and-muigi-pipe-problem.js
+++ codewars/8-kyu/;ario-and-muigi-pipe-problem.js
@@ -9,6 +9,7 @@
 // Given the a list of numbers, return a fixed list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
 
 // Example
+
 // Input:  1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
 
 function pipeFix(numbers){
