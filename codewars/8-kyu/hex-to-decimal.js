0cf1109bc88d3c0e106df5d79a284b385d82eb95
Marcus Izaquias <mizaquias@hotmail.co.uk>
2023-03-18 23:24:23 +0000

    8 kyu solution

--- codewars/8-kyu/hex-to-decimal.js
+++ codewars/8-kyu/hex-to-decimal.js
@@ -1,4 +1,4 @@
-// Complete the function which converts hex number (given as a string) to a decimal number.
+// Complete the function which converts hex number (given as a string) to a decimal numberr.
 
 function hexToDec(hexString){
  return  parseInt(hexString, 16);
