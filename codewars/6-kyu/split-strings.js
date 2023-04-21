f4f0752af1d871df2a43e8ad1d15afb852dedf1a
Marcus Izaquias <mizaquias@hotmail.co.uk>
2023-04-21 14:14:23 +0100

    6 kyu solution

---
+++ codewars/6-kyu/split-strings.js
@@ -0,0 +1,14 @@
+// Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+// Examples:
+
+// * 'abc' =>  ['ab', 'c_']
+// * 'abcdef' => ['ab', 'cd', 'ef']
+
+let solution = str => {
+  if (str === '') {
+    return []
+  } else {
+      return str.match(/.{1,2}/g).map(string => string.length === 1 ? string + '_' : string)
+    }
+}
