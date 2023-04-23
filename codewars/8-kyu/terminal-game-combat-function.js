7c947266ecb318fa9152269c82dd4a550ae3001f
Marcus Izaquias <mizaquias@hotmail.co.uk>
2023-04-23 14:19:19 +0100

    8 kyu solution

---
+++ codewars/8-kyu/terminal-game-combat-function.js
@@ -0,0 +1,16 @@
+// Create a combat function that takes the player's current health and the amount of damage recieved, 
+// and returns the player's new health. Health can't be less than 0.
+
+function combat(health, damage) {
+    if (health - damage < 0) {
+      return Math.abs(0)
+    } else {
+      return health - damage
+    }
+}
+
+// Using ternary
+
+function combat(health, damage) {
+	return health < damage ? 0 : health - damage
+}
